878d002fd3e24058890ac979a7db5dde761c7a70
Jackson Chen <teacher144123@gmail.com>
2017-12-21 12:29:28 +0800

    edit the framework

--- test_card.c
+++ test_card.c
@@ -10,6 +10,7 @@ typedef struct Card Card;
 
 // functional tool
 Card makeCard(char f, int p);
+void swapCard(Card *a, Card *b);
 void sortCards(Card user[13]);
 // cards
 void createAllCard(Card cards[52]);
@@ -17,10 +18,13 @@ void printCards(Card cards[52],int len);
 // init
 void initUser(Card cards[52],Card user[13]);
 void initCard(Card cards[52],Card user[13]);
+// card and cardset
+int cardsBig(Card a, Card b);
+void chooseCardSet(Card user[13]){
+	
+}
 // game
 void game();
-
-
 
 int main(int argc, char const *argv[]) {
 	game();
@@ -28,24 +32,10 @@ int main(int argc, char const *argv[]) {
 }
 
 
-
+// functional tool
 Card makeCard(char f, int p){
 	Card temp = {.flow = f, .point = p};
 	return temp;
-}
-int cardsBig(Card a, Card b){
-
-	if(a.point != b.point) return a.point > b.point;
-	else{
-		char big[4] = {'S','H','D','C'};
-		int f1, f2;
-		for(int i = 0;i < 4;++i){
-			if(a.flow == big[i])f1 = i;
-			if(b.flow == big[i])f2 = i;
-		}
-
-		return f1 > f2;
-	}
 }
 void swapCard(Card *a, Card *b){
 	Card temp = *a;
@@ -63,6 +53,7 @@ void sortCards(Card user[13]){
 	}
 }
 
+// cards
 void createAllCard(Card cards[52]){
 	int i;
 	for(i = 0;i < 13;++i)cards[i] = makeCard('S',i % 13 + 1);
@@ -70,7 +61,6 @@ void createAllCard(Card cards[52]){
 	for(i = 26;i < 39;++i)cards[i] = makeCard('D',i % 13 + 1);
 	for(i = 39;i < 52;++i)cards[i] = makeCard('C',i % 13 + 1);
 }
-
 void printCards(Card cards[52],int len){
 	int i;
 	for(i = 0;i < len;++i){
@@ -85,6 +75,7 @@ void printCards(Card cards[52],int len){
 	putchar('\n');
 }
 
+// init
 void initUser(Card cards[52],Card user[13]){
 	char got[52] = {0};		// using char to simulate bool save some memory
 	for(int i = 0;i < 13;++i){
@@ -95,17 +86,33 @@ void initUser(Card cards[52],Card user[13]){
 	}
 	sortCards(user);
 }
-
 void initCard(Card cards[52],Card user[13]){
 	createAllCard(cards);
 	initUser(cards,user);
 }
 
+// card and cardset
+int cardsBig(Card a, Card b){
+
+	if(a.point != b.point) return a.point > b.point;
+	else{
+		char big[4] = {'S','H','D','C'};
+		int f1, f2;
+		for(int i = 0;i < 4;++i){
+			if(a.flow == big[i])f1 = i;
+			if(b.flow == big[i])f2 = i;
+		}
+
+		return f1 > f2;
+	}
+}
 void chooseCardSet(Card user[13]){
     int a = 3;
     // testing
 }
 
+
+// game
 void game(){
 	srand(time(NULL));
 	Card all_cards[52], player1[13];
